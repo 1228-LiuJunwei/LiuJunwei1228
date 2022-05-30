c5b704e992d8e680341185bc995f2b73ef960ceb
1228-LiuJunwei <2904337785@qq.com>
2022-05-30 11:45:30 +0800

    week 11

---
+++ src/main/java/com/LiuJunwei/week11/demo/Dog.java
@@ -0,0 +1,12 @@
+package com.LiuJunwei.week11.demo;
+
+public class Dog {
+    private String name;
+    public void setName(String name){
+        this.name =name;
+    }
+    public String getName(){
+        return  name;
+    }
+
+}
